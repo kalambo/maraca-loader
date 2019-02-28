af0da2d8aea0b6498894763c8e373422f0c59d10
Jon Whitehead <jsiwhitehead@gmail.com>
2019-02-28 12:49:36 +0000

    Output parsed maraca config

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,20 @@
-import * as fs from 'fs-extra';
-import * as path from 'path';
+import { parse } from 'maraca';
 
 const requireId = () => `xxxREQUIRExxx${Math.random()}${Math.random()}xxx`;
 
-export default async function() {
-  const cb = this.async();
-  try {
-    const index = this.resourcePath
-      .split('\\')
-      .pop()
-      .split('/')
-      .pop();
-    const folder = this.resourcePath.slice(0, -index.length);
-    const fileNames = (await fs.readdir(folder)).filter(f => f.endsWith('.ma'));
-    fileNames.forEach(f => this.addDependency(path.join(folder, f)));
-    const dependencies = {};
-    const modules = (await Promise.all<string>(
-      fileNames.map(f => fs.readFile(path.join(folder, f), 'utf-8')),
-    )).reduce(
-      (res, file, i) => ({
-        ...res,
-        [fileNames[i].slice(0, -3)]: file.replace(
-          /image:\s*"([^"]+)"/g,
-          (_, image) => {
-            if (image.startsWith('http')) return `image: "${image}"`;
-            const id = requireId();
-            dependencies[id] = image;
-            return `image: "${id}"`;
-          },
-        ),
-      }),
-      {},
-    );
-    cb(
-      null,
-      `export default ${JSON.stringify(modules)}`.replace(
-        /xxxREQUIRExxx[0-9\.]+xxx/g,
-        id => {
-          const path = dependencies[id];
-          return '" + require(' + JSON.stringify(path) + ') + "';
-        },
-      ),
-    );
-  } catch (error) {
-    cb(error);
-  }
+export default function(file) {
+  const dependencies = {};
+  const result = file.replace(/image:\s*"([^"]+)"/g, (_, image) => {
+    if (image.startsWith('http')) return `image: "${image}"`;
+    const id = requireId();
+    dependencies[id] = image;
+    return `image: "${id}"`;
+  });
+  return `export default ${JSON.stringify(parse(result))}`.replace(
+    /xxxREQUIRExxx[0-9\.]+xxx/g,
+    id => {
+      const path = dependencies[id];
+      return '" + require(' + JSON.stringify(path) + ') + "';
+    },
+  );
 }
