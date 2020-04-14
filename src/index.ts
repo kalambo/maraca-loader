af89d76ed897740d2f8457205499fdbe228e50af
Jon Whitehead <jsiwhitehead@gmail.com>
2020-04-14 17:36:46 +0100

    Changed image reference format to html

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ const requireId = () => `xxxREQUIRExxx${Math.random()}${Math.random()}xxx`;
 
 export default function(file) {
   const dependencies = {};
-  const result = file.replace(/image:\s*"([^"]+)"/g, (_, image) => {
-    if (image.startsWith('http')) return `image: "${image}"`;
+  const result = file.replace(/src:\s*'([^']+)'/g, (_, image) => {
+    if (image.startsWith('http')) return `src: '${image}'`;
     const id = requireId();
     dependencies[id] = image;
-    return `image: "${id}"`;
+    return `src: '${id}'`;
   });
   return `export default ${JSON.stringify(parse(result))}`.replace(
     /xxxREQUIRExxx[0-9\.]+xxx/g,
     id => {
       const path = dependencies[id];
-      return '" + require(' + JSON.stringify(path) + ') + "';
+      return '" + require(' + JSON.stringify(path) + ').default + "';
     },
   );
 }
